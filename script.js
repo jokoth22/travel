e949ffaafc23243f574d0fc7ee3ccaf4a278bf24
jokoth22 <joyce.okoth22@gmail.com>
2023-10-08 12:42:34 -0400

    changed results code on js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+//const api_key= "JLVE704tRGtZrHRvY9Aa521QwfWD5eTzvRcTUw66";
+//https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=INSERT-API-KEY-HERE'
+
+//TODO: fix bug adding all parks to the list instead of page refresh-V code
+
+
+var showNationalParks = function(parks){
+    let results = document.getElementById("results");
+    results.innerHTML = ''; //clears previous history
+
+    for (let i=0;i<parks.length;i++){ 
+        
+        let park = document.createElement("div")
+        park.innerHTML = [
+            `<strong>${parks[i].fullName}</strong>`,
+            `<p>${parks[i].description}</p>`
+        ].join("")
+        results.appendChild(park) //should display not append
+    }
+}
+
+/*
 var showNationalParks = function(parks){
     console.log(parks)
     let results = document.getElementById("results");
@@ -7,9 +29,10 @@ var showNationalParks = function(parks){
             `<strong>${parks[i].fullName}</strong>`,
             `<p>${parks[i].description}</p>`
         ].join("")
-        results.append(park)
+        results.append(park) //should display not append
     }
 }
+*/
 
 let getNationalParks = function(state){
     var apiUrl = `https://developer.nps.gov/api/v1/parks?stateCode=${state}&api_key=ORm0C1iPEzxladrhfvdeAK32TYKBEaXewFngkMZp`
